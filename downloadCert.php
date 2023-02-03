5145fbd29e40888d23685cf62f63bdbe8f9982fc
Evgeny Kampiler <kampiler@gmail.com>
2023-02-04 02:48:13 +0300

    Update downloadCert.php

--- downloadCert.php
+++ downloadCert.php
@@ -1,7 +1,7 @@
 <?
   $out_dir='fssp_cer';
   @mkdir($out_dir);
-  $site = 'https://fssp.gov.ru';
+  $site = 'https://uc.fssp.gov.ru';
   $url = "$site/sertif/";
   $html = file_get_contents($url);#extension=php_openssl.dll
   #var_dump($html);
@@ -15,9 +15,11 @@
      $u=$node->getAttribute('href');
      if((strpos($u,'.cer'))or(strpos($u,'.crl')))
        {
+        echo "try - $u:\n";
         $fn=basename($u);
-        echo "Download $site$u --> to: $out_dir/$fn\n";
-        if($t=file_get_contents($site.$u))
+        if(strpos($u,'http')===false) $u=$site.$u;
+        echo "Download $u --> to: $out_dir/$fn\n";
+        if($t=file_get_contents($u))
           {
            file_put_contents("$out_dir/$fn",$t);
           }
